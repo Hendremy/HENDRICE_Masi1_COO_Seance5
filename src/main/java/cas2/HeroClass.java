563cf00bb6fee6ba25fc922992fc7b509878e952
superremy <hendriceremy@gmail.com>
2023-10-22 21:36:19 +0200

    Remove files of gitignore

--- src/main/java/cas2/HeroClass.java
+++
@@ -1,9 +0,0 @@
-package cas2;
-
-public class HeroClass {
-    public final static String FIGHTER = "Fighter";
-    public final static String MAGE = "Mage";
-    public final static String PRIEST = "Priest";
-    public final static String HUNTER = "Hunter";
-
-}
--- src/main/java/cas2/Race.java
+++
@@ -1,7 +0,0 @@
-package cas2;
-
-public class Race {
-    public final static String ELF = "Elf";
-    public final static String HUMAN = "Human";
-    public final static String ORC = "Orc";
-}
