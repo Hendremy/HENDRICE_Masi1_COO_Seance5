be842da29d29221bb3f7ae0f0f3db3b8548348a1
superremy <hendriceremy@gmail.com>
2023-10-22 22:42:18 +0200

    Cas 1 done

--- README.md
+++ README.md
@@ -1,12 +1,14 @@
 # Cas 1
 ## Design Pattern - Builder
 
+Ref: https://refactoring.guru/design-patterns/builder
+https://www.digitalocean.com/community/tutorials/java-httpurlconnection-example-java-http-request-get-post
 
 # Cas 2
 
 ## Design Pattern - Builder
 
-
+Ref: https://refactoring.guru/design-patterns/builder
 
 # Cas 3
 
--- src/main/java/cas1/Cas1.java
+++ src/main/java/cas1/Cas1.java
@@ -1,16 +1,55 @@
 package cas1;
 
-import org.apache.http.client.methods.HttpUriRequest;
 import org.apache.http.client.utils.URIBuilder;
-import org.apache.http.client.utils.URIUtils;
+import org.json.JSONObject;
+import org.json.JSONTokener;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 
 public class Cas1 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
+        int inkscape = 3472737;
+        int gitlabFoss = 13083;
+
+        System.out.println("Inkscape : " + getDescription(inkscape));
+        System.out.println("Gitlab FOSS : " + getDescription(gitlabFoss));
+    }
+
+    private static String getDescription(int projectId) throws Exception{
+        String path = "api/v4/projects";
+
         URIBuilder uriBuilder = new URIBuilder()
-                .setScheme("http")
-                .setHost("gitlab.com")
-                .setPath("/api/v/projects" + "")
-                ;
-        System.out.println("Hello world!");
+                .setScheme("https")
+                .setHost("gitlab.com");
+
+        URL url = uriBuilder.setPath(String.format("/%s/%d", path, projectId)).build().toURL();
+
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        connection.setRequestMethod("GET");
+        int responseCode = connection.getResponseCode();
+
+        if (responseCode == 200) { // success
+            BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            String inputLine;
+            StringBuffer response = new StringBuffer();
+
+            while ((inputLine = in.readLine()) != null) {
+                response.append(inputLine);
+            }
+            in.close();
+
+            // print result
+
+            JSONTokener tokener = new JSONTokener(response.toString());
+            JSONObject root = new JSONObject(tokener);
+
+            return root.getString("description");
+        } else {
+            return "GET request failed";
+        }
     }
 }
